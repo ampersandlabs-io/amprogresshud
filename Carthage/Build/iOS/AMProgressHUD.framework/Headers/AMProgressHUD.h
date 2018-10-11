c242d708508a49d6e1c4329a53457932888c39d3
Drew Barnes <djblin@gmail.com>
2018-10-11 14:23:16 +0000

    Setup Carthage

---
+++ Carthage/Build/iOS/AMProgressHUD.framework/Headers/AMProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  AMProgressHUD.h
+//  AMProgressHUD
+//
+//  Created by Drew Barnes on 10/10/2018.
+//  Copyright © 2018 Ampersand Technologies Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMProgressHUD.
+FOUNDATION_EXPORT double AMProgressHUDVersionNumber;
+
+//! Project version string for AMProgressHUD.
+FOUNDATION_EXPORT const unsigned char AMProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMProgressHUD/PublicHeader.h>
+
+
